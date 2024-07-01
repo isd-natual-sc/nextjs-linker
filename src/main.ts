d01f10ff464d2c3024930f4bee25f9b6f20635e2
isd-natural-sc <isd.nAndH1@gmail.com>
2024-07-01 17:15:16 +0900

    ないです

---
+++ src/main.ts
@@ -0,0 +1,62 @@
+interface HashMap<T> {
+  [key: string]: T;
+}
+
+const sheets = SpreadsheetApp.getActiveSpreadsheet().getSheets();
+
+const getSheetData = (): HashMap<string>[] => {
+  const dataList: HashMap<string>[] = [];
+  const data: HashMap<string> = {};
+  for (const sheet of sheets) {
+    const lastRow = sheet.getLastRow();
+
+    const keys = sheet
+      .getRange("B2:G2")
+      .getValues()[0]
+      // 型変換
+      .map((k) => String(k));
+
+    const names = sheet
+      .getRange(`B3:B${lastRow}`)
+      .getValues()[0]
+      // 型変換
+      .map((p) => String(p));
+
+    const datums = sheet
+      .getRange(`C3:G${lastRow}`)
+      .getValues()
+      // 型変換
+      .map((d) => d.map((ind) => String(ind)));
+
+    for (const keyIndex in datums) {
+      for (const dataIndex in datums[keyIndex]) {
+        const key = keys[dataIndex];
+
+        data[key] = datums[keyIndex][dataIndex];
+      }
+
+      dataList.push(data);
+    }
+  }
+
+  return dataList;
+};
+
+const sheetGet = (e: any) => {
+  const callback = e.parameter.callback;
+  const sheetData = getSheetData();
+  let out: GoogleAppsScript.Content.TextOutput;
+
+  if (callback) {
+    const text = `${callback}(${JSON.stringify(sheetData, null, 2)})`;
+    out = ContentService.createTextOutput(text);
+    out.setMimeType(ContentService.MimeType.JAVASCRIPT);
+  } else {
+    out = ContentService.createTextOutput();
+    out.setMimeType(ContentService.MimeType.JSON);
+    // JSONをセット
+    out.setContent(JSON.stringify(sheetData));
+  }
+
+  return out;
+};
